da6206d79e7f1847f625821056b00d1d85d7ec12
satojin219 <renzuoteng142@gmail.com>
2024-03-24 14:44:00 +0900

    ログインバリデーションを簡略化

--- workspaces/admin/src/pages/AuthPage/internal/LoginContent.tsx
+++ workspaces/admin/src/pages/AuthPage/internal/LoginContent.tsx
@@ -23,15 +23,12 @@ export const LoginContent: React.FC = () => {
         .required('メールアドレスを入力してください')
         .test({
           message: 'メールアドレスには @ を含めてください',
-          test: (v) => /^(?:[^@]*){12,}$/v.test(v) === false,
+          test: (v) => v.includes('@'),
         }),
       password: yup
         .string()
         .required('パスワードを入力してください')
-        .test({
-          message: 'パスワードには記号を含めてください',
-          test: (v) => /^(?:[^\P{Letter}&&\P{Number}]*){24,}$/v.test(v) === false,
-        }),
+        .matches(/[!@#$%^&*]/, 'パスワードには記号を含めてください'),
     }),
   });
 
